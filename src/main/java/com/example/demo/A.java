16c982c5200e088256c742e87b3b444cc6523fbe
doik <doik.jang@cardoc.kr>
2020-06-26 11:55:37 +0900

    feature8 완료

--- src/main/java/com/example/demo/A.java
+++ src/main/java/com/example/demo/A.java
@@ -1,6 +1,9 @@
 package com.example.demo;
 
 public class A {
+    public static void feature6() {
+        System.out.println("feature6");
+    }
     public static void feature8(){
         System.out.println("feature8");
     }
